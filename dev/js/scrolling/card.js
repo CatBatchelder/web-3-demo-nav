945179ccdc9f9479f0d4a7a36379f409a674a428
CatBatchelder <63326220+CatBatchelder@users.noreply.github.com>
2020-08-03 17:47:05 -0400

    making the card actually move

--- dev/js/scrolling/card.js
+++ dev/js/scrolling/card.js
@@ -12,11 +12,16 @@ var cardAnimationTimeLine = gsap.timeline({
 //start the timeline
 cardAnimationTimeLine.addLabel("card to slide in")
                     .from("#card",{duration:1, x:150})
+                    .addLabel("logo to fade in")
+                    .from("#card-logo",{duration:1, alpha:0}, "-=.5")
+                    .addLabel("text moving")
+                    .from(".card-text", {duration:1, alpha:0, y:20})
 
+//making the scroll trigger actually work when you scroll
 ScrollTrigger.create({
     trigger: "#card",
     start: "top 90%",
     end: "bottom 50%",
-    markers: true,
+    //markers: true,
     animation: cardAnimationTimeLine
 })
